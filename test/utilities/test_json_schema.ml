71da0f402fbe8fd47331d21ce525cc8892a21560
Norwen Gonio <neu.gonio@gmail.com>
2025-07-04 21:42:00 +0800

    fix json_schema test

--- test/utilities/test_json_schema.ml
+++ test/utilities/test_json_schema.ml
@@ -1,146 +1,198 @@
-open Alcotest
-open Yojson.Safe
-open Utilities.Json_schema
+open! Core
+open! Async
+open! Json_schema
+open! Expect_test_helpers_core
 
-let test_schema = `Assoc [
-  ("type", `String "object");
-  ("properties", `Assoc [
-    ("name", `Assoc [
-      ("type", `String "string");
-      ("title", `String "Name");
-    ]);
-    ("age", `Assoc [
-      ("type", `String "integer");
-      ("title", `String "Age");
-    ]);
-  ]);
-  ("required", `List [`String "name"; `String "age"]);
-  ("additionalProperties", `Bool false);
-  ("$defs", `Assoc [
-    ("unused_def", `Assoc [
-      ("type", `String "string")
-    ]);
-    ("used_def", `Assoc [
-      ("type", `String "object")
-    ]);
-  ]);
-]
+let rec sexp_of_yojson (json : Yojson.Safe.t) : Sexp.t =
+  match json with
+  | `Null -> Sexp.Atom "null"
+  | `Bool b -> Sexp.Atom (Bool.to_string b)
+  | `Int i -> Sexp.Atom (Int.to_string i)
+  | `Float f -> Sexp.Atom (Float.to_string f)
+  | `String s -> Sexp.Atom s
+  | `List xs -> Sexp.List (List.map xs ~f:sexp_of_yojson)
+  | `Assoc xs -> 
+    Sexp.List (List.map xs ~f:(fun (k, v) -> 
+      Sexp.List [Sexp.Atom k; sexp_of_yojson v]))
+  | `Intlit s -> Sexp.Atom s
+  | `Tuple _ | `Variant _ -> failwith "Unsupported JSON type"
 
-let schema_with_ref = `Assoc [
-  ("type", `String "object");
-  ("properties", `Assoc [
-    ("person", `Assoc [
-      ("$ref", `String "#/$defs/used_def")
-    ])
-  ])
-]
+let print_json json =
+  print_s (sexp_of_yojson json);
+  return ()
 
-let test_prune_param () =
-  let schema = test_schema in
-  let pruned = prune_param schema "age" in
-  let props = match pruned |> Util.member "properties" with
-    | `Assoc props -> props
-    | _ -> [] in
-  let required = match pruned |> Util.member "required" with
-    | `List required -> required
-    | _ -> [] in
-  
-  (* Check age was removed from properties *)
-  check bool "age removed from properties"
-    false (List.mem_assoc "age" props);
-  
-  (* Check age was removed from required *)
-  check bool "age removed from required"
-    false (List.exists (fun x -> x = `String "age") required)
+let%expect_test "test_prune_param_nonexistent" =
+  let schema = `Assoc [
+    "properties", `Assoc [
+      "foo", `Assoc ["type", `String "string"]
+    ]
+  ] in
+  let%bind () = 
+    let result = prune_param schema "bar" in
+    print_json result
+  in
+  [%expect {| ((properties ((foo ((type string)))))) |}];
+  return ()
 
-let test_prune_unused_defs () =
-  (* Combine schemas by merging their properties and keeping defs from test_schema *)
-  let combined = match test_schema, schema_with_ref with
-    | `Assoc fields1, `Assoc fields2 ->
-        let merged_props = match List.assoc_opt "properties" fields1, List.assoc_opt "properties" fields2 with
-          | Some (`Assoc props1), Some (`Assoc props2) ->
-              ("properties", `Assoc (props1 @ props2))
-          | _ -> ("properties", `Assoc []) in
-        let fields = List.filter (fun (k, _) -> k <> "properties") fields1 in
-        `Assoc (merged_props :: fields)
-    | _ -> test_schema in
-  
-  let pruned = prune_unused_defs combined in
-  let defs = match pruned |> Util.member "$defs" with
-    | `Assoc defs -> defs
-    | _ -> [] in
-  
-  (* Check unused_def was removed *)
-  check bool "unused_def removed"
-    false (List.mem_assoc "unused_def" defs);
-  
-  (* Check used_def was kept *)
-  check bool "used_def kept"
-    true (List.mem_assoc "used_def" defs)
+let%expect_test "test_prune_param_exists" =
+  let schema = `Assoc [
+    "properties", `Assoc [
+      "foo", `Assoc ["type", `String "string"];
+      "bar", `Assoc ["type", `String "integer"]
+    ]
+  ] in
+  let%bind () = 
+    let result = prune_param schema "bar" in
+    print_json result
+  in
+  [%expect {| ((properties ((foo ((type string)))))) |}];
+  return ()
 
-let test_walk_and_prune () =
-  let schema = test_schema in
-  let pruned = walk_and_prune ~prune_titles:true ~prune_additional_properties:true schema in
-  
-  (* Check titles were removed *)
-  let has_title = ref false in
-  let rec check_title = function
-    | `Assoc fields ->
-        if List.mem_assoc "title" fields then
-          has_title := true
-        else
-          List.iter (fun (_, v) -> check_title v) fields
-    | `List items ->
-        List.iter check_title items
-    | _ -> () in
-  check_title pruned;
-  check bool "titles removed" false !has_title;
-  
-  (* Check additionalProperties was removed *)
-  check bool "additionalProperties removed"
-    false (match pruned with
-      | `Assoc fields -> List.mem_assoc "additionalProperties" fields
-      | _ -> true)
+let%expect_test "test_prune_param_last_property" =
+  let schema = `Assoc [
+    "properties", `Assoc [
+      "foo", `Assoc ["type", `String "string"]
+    ]
+  ] in
+  let%bind () = 
+    let result = prune_param schema "foo" in
+    print_json result
+  in
+  [%expect {| ((properties ())) |}];
+  return ()
 
-let test_compress_schema () =
-  let schema = test_schema in
-  let compressed = compress_schema
-    ~prune_params:["age"]
-    ~prune_defs:true
-    ~prune_additional_properties:true
-    ~prune_titles:true
-    schema in
-  
-  (* Check all pruning operations were applied *)
-  let props = match compressed |> Util.member "properties" with
-    | `Assoc props -> props
-    | _ -> [] in
-  check bool "age removed" false (List.mem_assoc "age" props);
-  
-  let has_title = ref false in
-  let rec check_title = function
-    | `Assoc fields ->
-        if List.mem_assoc "title" fields then
-          has_title := true
-        else
-          List.iter (fun (_, v) -> check_title v) fields
-    | `List items ->
-        List.iter check_title items
-    | _ -> () in
-  check_title compressed;
-  check bool "titles removed" false !has_title;
-  
-  check bool "additionalProperties removed"
-    false (match compressed with
-      | `Assoc fields -> List.mem_assoc "additionalProperties" fields
-      | _ -> true)
+let%expect_test "test_prune_param_from_required" =
+  let schema = `Assoc [
+    "properties", `Assoc [
+      "foo", `Assoc ["type", `String "string"];
+      "bar", `Assoc ["type", `String "integer"]
+    ];
+    "required", `List [`String "foo"; `String "bar"]
+  ] in
+  let%bind () = 
+    let result = prune_param schema "bar" in
+    print_json result
+  in
+  [%expect {| ((properties ((foo ((type string))))) (required (foo))) |}];
+  return ()
 
-let () =
-  run "JSON Schema Tests" [
-    ("JSON Schema", [
-      test_case "Prune parameter" `Quick test_prune_param;
-      test_case "Prune unused definitions" `Quick test_prune_unused_defs;
-      test_case "Walk and prune" `Quick test_walk_and_prune;
-      test_case "Compress schema" `Quick test_compress_schema;
-    ]);
-  ] 
+let%expect_test "test_prune_param_last_required" =
+  let schema = `Assoc [
+    "properties", `Assoc [
+      "foo", `Assoc ["type", `String "string"];
+      "bar", `Assoc ["type", `String "integer"]
+    ];
+    "required", `List [`String "foo"]
+  ] in
+  let%bind () = 
+    let result = prune_param schema "foo" in
+    print_json result
+  in
+  [%expect {| ((properties ((bar ((type integer)))))) |}];
+  return ()
+
+let%expect_test "test_prune_unused_defs_removes_unreferenced" =
+  let schema = `Assoc [
+    "properties", `Assoc [
+      "foo", `Assoc ["$ref", `String "#/$defs/foo_def"]
+    ];
+    "$defs", `Assoc [
+      "foo_def", `Assoc ["type", `String "string"];
+      "unused_def", `Assoc ["type", `String "integer"]
+    ]
+  ] in
+  let%bind () = 
+    let result = prune_unused_defs schema in
+    print_json result
+  in
+  [%expect {|
+    ((properties ((foo (($ref #/$defs/foo_def)))))
+     ($defs ((foo_def ((type string))))))
+    |}];
+  return ()
+
+let%expect_test "test_prune_unused_defs_nested_references_kept" =
+  let schema = `Assoc [
+    "properties", `Assoc [
+      "foo", `Assoc ["$ref", `String "#/$defs/foo_def"]
+    ];
+    "$defs", `Assoc [
+      "foo_def", `Assoc [
+        "type", `String "object";
+        "properties", `Assoc [
+          "nested", `Assoc ["$ref", `String "#/$defs/nested_def"]
+        ]
+      ];
+      "nested_def", `Assoc ["type", `String "string"];
+      "unused_def", `Assoc ["type", `String "integer"]
+    ]
+  ] in
+  let%bind () = 
+    let result = prune_unused_defs schema in
+    print_json result
+  in
+  [%expect {|
+    ((properties ((foo (($ref #/$defs/foo_def)))))
+     ($defs (
+       (foo_def (
+         (type object) (properties ((nested (($ref #/$defs/nested_def)))))))
+       (nested_def ((type string))))))
+    |}];
+  return ()
+
+let%expect_test "test_walk_and_prune_additional_properties" =
+  let schema = `Assoc [
+    "type", `String "object";
+    "properties", `Assoc ["foo", `Assoc ["type", `String "string"]];
+    "additionalProperties", `Bool false
+  ] in
+  let%bind () = 
+    let result = walk_and_prune ~prune_additional_properties:true schema in
+    print_json result
+  in
+  [%expect {| ((type object) (properties ((foo ((type string)))))) |}];
+  return ()
+
+let%expect_test "test_walk_and_prune_titles" =
+  let schema = `Assoc [
+    "title", `String "Root Schema";
+    "type", `String "object";
+    "properties", `Assoc [
+      "foo", `Assoc [
+        "title", `String "Foo Property";
+        "type", `String "string"
+      ]
+    ]
+  ] in
+  let%bind () = 
+    let result = walk_and_prune ~prune_titles:true schema in
+    print_json result
+  in
+  [%expect {| ((type object) (properties ((foo ((type string)))))) |}];
+  return ()
+
+let%expect_test "test_compress_schema_all_features" =
+  let schema = `Assoc [
+    "type", `String "object";
+    "title", `String "Root Schema";
+    "properties", `Assoc [
+      "keep", `Assoc ["type", `String "string"];
+      "remove", `Assoc ["$ref", `String "#/$defs/remove_def"]
+    ];
+    "required", `List [`String "keep"; `String "remove"];
+    "additionalProperties", `Bool false;
+    "$defs", `Assoc [
+      "remove_def", `Assoc ["type", `String "string"];
+      "unused_def", `Assoc ["type", `String "number"]
+    ]
+  ] in
+  let%bind () = 
+    let result = compress_schema 
+        ~prune_params:["remove"] 
+        ~prune_titles:true 
+        ~prune_additional_properties:true 
+        schema in
+    print_json result
+  in
+  [%expect {| ((type object) (properties ((keep ((type string))))) (required (keep))) |}];
+  return () 
